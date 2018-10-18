375902617830f13fb2e37299dd2cf587e76723b8
CarlosLabrado <carlos@petrolog.us>
2018-10-18 12:55:04 -0600

    Test: Token refresh and class refactor.

--- main.py
+++ main.py
@@ -1,179 +1,191 @@
 import pyrebase
 import os
-
-email = os.environ['email']
-password = os.environ['password']
-
-config = {
-    "apiKey": os.environ['apiKey'],
-    "authDomain": os.environ['authDomain'],
-    "databaseURL": os.environ['databaseURL'],
-    "projectId": os.environ['projectId'],
-    "storageBucket": os.environ['storageBucket'],
-    "messagingSenderId": os.environ['messagingSenderId']
-}
-
-firebase = pyrebase.initialize_app(config)
-
-# Get a reference to the auth service
-auth = firebase.auth()
-
-# Log the user in
-user = auth.sign_in_with_email_and_password(email, password)
-
-# Get a reference to the database service
-db = firebase.database()
+import arrow
 
 
-def firebase_post(button):
-    firebase_data = {
-        "buttonPressed": button
+class Main:
+    email = os.environ['email']
+    password = os.environ['password']
+
+    token_date = arrow.utcnow()
+
+    config = {
+        "apiKey": os.environ['apiKey'],
+        "authDomain": os.environ['authDomain'],
+        "databaseURL": os.environ['databaseURL'],
+        "projectId": os.environ['projectId'],
+        "storageBucket": os.environ['storageBucket'],
+        "messagingSenderId": os.environ['messagingSenderId']
     }
-    db.child("testButtons").update(firebase_data, user['idToken'])
 
+    firebase = pyrebase.initialize_app(config)
 
-import RPi.GPIO as GPIO
+    # Get a reference to the auth service
+    auth = firebase.auth()
 
-import time
+    # Log the user in
+    user = auth.sign_in_with_email_and_password(email, password)
 
-import Adafruit_GPIO.SPI as SPI
-import Adafruit_SSD1306
+    # Get a reference to the database service
+    db = firebase.database()
 
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
+    def firebase_post(self, button):
+        firebase_data = {
+            "buttonPressed": button
+        }
+        self.check_token_expired(self.user)
+        self.db.child("testButtons").update(firebase_data, self.user['idToken'])
 
-# Input pins:
-L_pin = 27
-R_pin = 23
-C_pin = 4
-U_pin = 17
-D_pin = 22
+    def check_token_expired(self, current_user):
+        now_date = arrow.utcnow()
+        delta_date = now_date - self.token_date
+        if delta_date.seconds >= 3559:  # if more than one hour refresh.
+            current_user = self.auth.refresh(current_user['refreshToken'])
+            # now we have a fresh token
+            self.token_date = now_date
+            return current_user
 
-A_pin = 5
-B_pin = 6
+    import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
+    import time
 
-GPIO.setup(A_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Input with pull-up
-GPIO.setup(B_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Input with pull-up
-GPIO.setup(L_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Input with pull-up
-GPIO.setup(R_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Input with pull-up
-GPIO.setup(U_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Input with pull-up
-GPIO.setup(D_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Input with pull-up
-GPIO.setup(C_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Input with pull-up
+    import Adafruit_GPIO.SPI as SPI
+    import Adafruit_SSD1306
 
-# Raspberry Pi pin configuration:
-RST = 24
-# Note the following are only used with SPI:
-DC = 23
-SPI_PORT = 0
-SPI_DEVICE = 0
+    from PIL import Image
+    from PIL import ImageDraw
+    from PIL import ImageFont
 
-# Beaglebone Black pin configuration:
-# RST = 'P9_12'
-# Note the following are only used with SPI:
-# DC = 'P9_15'
-# SPI_PORT = 1
-# SPI_DEVICE = 0
+    # Input pins:
+    L_pin = 27
+    R_pin = 23
+    C_pin = 4
+    U_pin = 17
+    D_pin = 22
 
-# 128x32 display with hardware I2C:
-# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
+    A_pin = 5
+    B_pin = 6
 
-# 128x64 display with hardware I2C:
-disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
+    GPIO.setmode(GPIO.BCM)
 
-# Note you can change the I2C address by passing an i2c_address parameter like:
-# disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3C)
+    GPIO.setup(A_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Input with pull-up
+    GPIO.setup(B_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Input with pull-up
+    GPIO.setup(L_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Input with pull-up
+    GPIO.setup(R_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Input with pull-up
+    GPIO.setup(U_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Input with pull-up
+    GPIO.setup(D_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Input with pull-up
+    GPIO.setup(C_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Input with pull-up
 
-# Alternatively you can specify an explicit I2C bus number, for example
-# with the 128x32 display you would use:
-# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, i2c_bus=2)
+    # Raspberry Pi pin configuration:
+    RST = 24
+    # Note the following are only used with SPI:
+    DC = 23
+    SPI_PORT = 0
+    SPI_DEVICE = 0
 
-# 128x32 display with hardware SPI:
-# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
+    # Beaglebone Black pin configuration:
+    # RST = 'P9_12'
+    # Note the following are only used with SPI:
+    # DC = 'P9_15'
+    # SPI_PORT = 1
+    # SPI_DEVICE = 0
 
-# 128x64 display with hardware SPI:
-# disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
+    # 128x32 display with hardware I2C:
+    # disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
 
-# Alternatively you can specify a software SPI implementation by providing
-# digital GPIO pin numbers for all the required display pins.  For example
-# on a Raspberry Pi with the 128x32 display you might use:
-# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, dc=DC, sclk=18, din=25, cs=22)
+    # 128x64 display with hardware I2C:
+    disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
 
-# Initialize library.
-disp.begin()
+    # Note you can change the I2C address by passing an i2c_address parameter like:
+    # disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3C)
 
-# Clear display.
-disp.clear()
-disp.display()
+    # Alternatively you can specify an explicit I2C bus number, for example
+    # with the 128x32 display you would use:
+    # disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, i2c_bus=2)
 
-# Create blank image for drawing.
-# Make sure to create image with mode '1' for 1-bit color.
-width = disp.width
-height = disp.height
-image = Image.new('1', (width, height))
+    # 128x32 display with hardware SPI:
+    # disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
 
-# Get drawing object to draw on image.
-draw = ImageDraw.Draw(image)
+    # 128x64 display with hardware SPI:
+    # disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
 
-# Draw a black filled box to clear the image.
-draw.rectangle((0, 0, width, height), outline=0, fill=0)
+    # Alternatively you can specify a software SPI implementation by providing
+    # digital GPIO pin numbers for all the required display pins.  For example
+    # on a Raspberry Pi with the 128x32 display you might use:
+    # disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, dc=DC, sclk=18, din=25, cs=22)
 
-try:
-    while 1:
-        if GPIO.input(U_pin):  # button is released
-            draw.polygon([(20, 20), (30, 2), (40, 20)], outline=255, fill=0)  # Up
-        else:  # button is pressed:
-            draw.polygon([(20, 20), (30, 2), (40, 20)], outline=255, fill=1)  # Up filled
-            firebase_post("up")
+    # Initialize library.
+    disp.begin()
 
-        if GPIO.input(L_pin):  # button is released
-            draw.polygon([(0, 30), (18, 21), (18, 41)], outline=255, fill=0)  # left
-        else:  # button is pressed:
-            draw.polygon([(0, 30), (18, 21), (18, 41)], outline=255, fill=1)  # left filled
-            firebase_post("left")
+    # Clear display.
+    disp.clear()
+    disp.display()
 
-        if GPIO.input(R_pin):  # button is released
-            draw.polygon([(60, 30), (42, 21), (42, 41)], outline=255, fill=0)  # right
-        else:  # button is pressed:
-            draw.polygon([(60, 30), (42, 21), (42, 41)], outline=255, fill=1)  # right filled
-            firebase_post("right")
+    # Create blank image for drawing.
+    # Make sure to create image with mode '1' for 1-bit color.
+    width = disp.width
+    height = disp.height
+    image = Image.new('1', (width, height))
 
-        if GPIO.input(D_pin):  # button is released
-            draw.polygon([(30, 60), (40, 42), (20, 42)], outline=255, fill=0)  # down
-        else:  # button is pressed:
-            draw.polygon([(30, 60), (40, 42), (20, 42)], outline=255, fill=1)  # down filled
-            firebase_post("down")
+    # Get drawing object to draw on image.
+    draw = ImageDraw.Draw(image)
 
-        if GPIO.input(C_pin):  # button is released
-            draw.rectangle((20, 22, 40, 40), outline=255, fill=0)  # center
-        else:  # button is pressed:
-            draw.rectangle((20, 22, 40, 40), outline=255, fill=1)  # center filled
-            firebase_post("center")
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
-        if GPIO.input(A_pin):  # button is released
-            draw.ellipse((70, 40, 90, 60), outline=255, fill=0)  # A button
-        else:  # button is pressed:
-            draw.ellipse((70, 40, 90, 60), outline=255, fill=1)  # A button filled
-            firebase_post("A")
+    try:
+        while 1:
+            if GPIO.input(U_pin):  # button is released
+                draw.polygon([(20, 20), (30, 2), (40, 20)], outline=255, fill=0)  # Up
+            else:  # button is pressed:
+                draw.polygon([(20, 20), (30, 2), (40, 20)], outline=255, fill=1)  # Up filled
+                firebase_post("up")
 
-        if GPIO.input(B_pin):  # button is released
-            draw.ellipse((100, 20, 120, 40), outline=255, fill=0)  # B button
-        else:  # button is pressed:
-            draw.ellipse((100, 20, 120, 40), outline=255, fill=1)  # B button filled
-            firebase_post("B")
+            if GPIO.input(L_pin):  # button is released
+                draw.polygon([(0, 30), (18, 21), (18, 41)], outline=255, fill=0)  # left
+            else:  # button is pressed:
+                draw.polygon([(0, 30), (18, 21), (18, 41)], outline=255, fill=1)  # left filled
+                firebase_post("left")
 
-        if not GPIO.input(A_pin) and not GPIO.input(B_pin) and not GPIO.input(C_pin):
-            catImage = Image.open('happycat_oled_64.ppm').convert('1')
-            disp.image(catImage)
-        else:
-            # Display image.
-            disp.image(image)
+            if GPIO.input(R_pin):  # button is released
+                draw.polygon([(60, 30), (42, 21), (42, 41)], outline=255, fill=0)  # right
+            else:  # button is pressed:
+                draw.polygon([(60, 30), (42, 21), (42, 41)], outline=255, fill=1)  # right filled
+                firebase_post("right")
 
-        disp.display()
-        time.sleep(.01)
+            if GPIO.input(D_pin):  # button is released
+                draw.polygon([(30, 60), (40, 42), (20, 42)], outline=255, fill=0)  # down
+            else:  # button is pressed:
+                draw.polygon([(30, 60), (40, 42), (20, 42)], outline=255, fill=1)  # down filled
+                firebase_post("down")
 
+            if GPIO.input(C_pin):  # button is released
+                draw.rectangle((20, 22, 40, 40), outline=255, fill=0)  # center
+            else:  # button is pressed:
+                draw.rectangle((20, 22, 40, 40), outline=255, fill=1)  # center filled
+                firebase_post("center")
 
-except KeyboardInterrupt:
-    GPIO.cleanup()
+            if GPIO.input(A_pin):  # button is released
+                draw.ellipse((70, 40, 90, 60), outline=255, fill=0)  # A button
+            else:  # button is pressed:
+                draw.ellipse((70, 40, 90, 60), outline=255, fill=1)  # A button filled
+                firebase_post("A")
+
+            if GPIO.input(B_pin):  # button is released
+                draw.ellipse((100, 20, 120, 40), outline=255, fill=0)  # B button
+            else:  # button is pressed:
+                draw.ellipse((100, 20, 120, 40), outline=255, fill=1)  # B button filled
+                firebase_post("B")
+
+            if not GPIO.input(A_pin) and not GPIO.input(B_pin) and not GPIO.input(C_pin):
+                catImage = Image.open('happycat_oled_64.ppm').convert('1')
+                disp.image(catImage)
+            else:
+                # Display image.
+                disp.image(image)
+
+            disp.display()
+            time.sleep(.01)
+
+    except KeyboardInterrupt:
+        GPIO.cleanup()
